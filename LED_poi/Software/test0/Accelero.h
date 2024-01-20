11c1bf7d90d9395cffbff1a8ca8ae9309e7cc996
reivax-boucoi <reivax.boucoi@laposte.net>
2024-01-20 22:02:30 +0100

    a

--- LED_poi/Software/test0/Accelero.h
+++ LED_poi/Software/test0/Accelero.h
@@ -6,18 +6,17 @@
 
 class ACC {
     private:
-        int16_t meanY;
         int16_t maxY;
         int16_t minY;
         int16_t value_f;
-        int16_t value;
-        bool newCycleFlag;
         uint8_t periodPos;
         uint8_t periodNeg;
         uint8_t cntPos;
         uint8_t cntNeg;
 
     public:
+        bool newHalfCycleFlag;
+        bool cycleState;
         ACC(uint8_t rate, uint8_t gees);
         int16_t getY();
         void printRaw();
