aafbd2efc9f2b5394e01560658c1d4f2106f6f8f
reivax-boucoi <reivax.boucoi@laposte.net>
2024-02-03 19:21:33 +0100

    a

---
+++ LED_poi/Software/brd_bringup/brd_bringup.ino
@@ -0,0 +1,22 @@
+
+#include <Adafruit_NeoPixel.h>
+
+
+#define PIXEL_PIN 13 //PC7
+#define PIXEL_COUNT 15
+
+uint32_t pixel_data[PIXEL_COUNT];
+Adafruit_NeoPixel led_strip(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
+uint8_t mode = 0;
+
+void setup() {
+    led_strip.begin(); // Initialize NeoPixel strip object (REQUIRED)
+    led_strip.show();  // Initialize all pixels to 'off'
+
+}
+
+void loop() {
+    led_strip.fill(0x000000, mode++, 0);
+    if (mode > 13)mode = 0;
+    led_strip.show();
+}
