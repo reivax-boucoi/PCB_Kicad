148904d79786b9c65de77c44cf9ee7a15e467a0d
reivax-boucoi <reivax.boucoi@laposte.net>
2024-02-08 19:13:22 +0100

    a

--- LED_poi/Software/test0/Animations.h
+++ LED_poi/Software/test0/Animations.h
@@ -10,4 +10,24 @@ void LEDs_init();
 void LEDs_setColor(uint32_t c);
 void LEDS_showMode(uint8_t mode);
 
+typedef void(*Anim)(uint8_t step);
+
+void Anim_blink(uint8_t step);
+void Anim_rainbow(uint8_t step);
+void Anim_blank(uint8_t step);
+
+class Animations {
+
+    private:
+        uint8_t animId;
+        uint8_t animStep;
+        uint8_t animSpeed;
+        bool isEnabled;
+        Anim animList[7];
+    public:
+        Animations(void);
+        void setAnim(uint8_t new_animId);
+        void update();
+};
+
 #endif
