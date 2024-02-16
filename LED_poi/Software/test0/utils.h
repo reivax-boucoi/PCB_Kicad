215dcc29de59ccbf46250347757a7a42988463b6
reivax-boucoi <reivax.boucoi@laposte.net>
2024-02-16 22:01:19 +0100

    a

--- LED_poi/Software/test0/utils.h
+++ LED_poi/Software/test0/utils.h
@@ -2,10 +2,11 @@
 #define utils_h
 
 #include <Arduino.h>
+#include "Animations.h"
 
+extern Animations* myAnimations;
 #define KEY 5           //PC6 (output, active high)
 #define BTN 10          //PB6 (input, active high)
-#define LED_ONBRD 7     //PE6 (output, active high)
 #define ACC_INT1 8      //PB4 (input, active high, 3.3V)
 #define ACC_INT2 9      //PB5 (input, active high, 3.3V)
 
