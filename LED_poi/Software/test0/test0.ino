03f755909c2cbd2ac82e321789a7023efa703a19
reivax-boucoi <reivax.boucoi@laposte.net>
2024-02-18 22:01:00 +0100

    a

--- LED_poi/Software/test0/test0.ino
+++ LED_poi/Software/test0/test0.ino
@@ -1,7 +1,7 @@
 #include "utils.h"
 #include "IR_cmd.h"
 #include "Accelero.h"
-#include "Animations.h"
+//#include "Animations.h"
 
 
 #define ACC_POLL_RATE 10 //in ms
@@ -9,7 +9,6 @@
 #define PSU_REFRESH_CNT (PSU_REFRESH_RATE*1000/ACC_POLL_RATE)
 
 boolean oldState = LOW;
-int mode = 0;    // Currently-active animation mode, 0-9
 uint32_t pTime = 0;
 uint16_t psu_refresh_counter = 0;
 ACC* acc;
@@ -19,36 +18,41 @@ void setup() {
     LEDs_init();
     acc = new ACC(200, 8);
     Serial.begin(115200);
-    /*  while (!Serial) {
-        ; // wait for serial port to connect. Needed for native USB port only
-        }*/
-    LEDs_setColor(0xF0F0F0);
+//    while (!Serial) {}
+    Serial.println("Hello!");
+    LEDs_setColor(0xFF0000);
+    _delay_ms(250);
+    LEDs_setColor(0x00FF00);
+    _delay_ms(250);
+    LEDs_setColor(0x0000FF);
+    _delay_ms(250);
+    LEDs_setColor(0x000000);
 }
 
 void loop() {
 
-    if (millis() - pTime > ACC_POLL_RATE) {
+      if (millis() - pTime > ACC_POLL_RATE) {
         pTime = millis();
         //      Serial.println(acc->getY());
         acc->getY();
         //acc->printRaw();
-        acc->printPeriod();
+        //acc->printPeriod();
         if(acc->newHalfCycleFlag==true){
             acc->newHalfCycleFlag=false;
             if(acc->cycleState==true){
-                LEDs_setColor(0x000000);
+                //LEDs_setColor(0x000000);
             }else{
-                LEDs_setColor(0xFF0000);                
+                //LEDs_setColor(0xFF0000);
             }
         }
-        digitalWrite(LED_ONBRD, !digitalRead(LED_ONBRD));
         //acc.print();
+        
         if (++psu_refresh_counter > PSU_REFRESH_CNT) {
             psu_refresh_counter = 0;
             supply_ON();
-//            myAnimations.update();
+            
         }
-
+        myAnimations->update();
 
         boolean newState = digitalRead(BTN);
         // Check if state changed from low to hi (button press).
@@ -58,46 +62,10 @@ void loop() {
             // Check if button is still low after debounce.
             newState = digitalRead(BTN);
             if (newState == HIGH) {     // Yes, still hi
-                if (++mode > 7) mode = 0;
-                Serial.println("Mode" + mode);
-                LEDS_showMode(mode);/*
-                switch (mode) {
-                    case 0:
-                        LEDs_setColor(0x0000FF);
-                        break;
-                    case 1:
-                        LEDs_setColor(0x00FF00);
-                        break;
-                    case 2:
-                        LEDs_setColor(0xFF0000);
-                        break;
-                    case 3:
-                        LEDs_setColor(0x00FFFF);
-                        break;
-                    case 4:
-                        LEDs_setColor(0xFFFF00);
-                        break;
-                    case 5:
-                        LEDs_setColor(0xFF00FF);
-                        break;
-                    case 6:
-                        LEDs_setColor(0xFFFFFF);
-                        break;
-                    case 7:
-                        LEDs_setColor(0x000000);
-                        break;
-                    case 8:
-                        supply_OFF();
-                        break;
-                    default:
-                        break;
-
-                }*/
-                digitalWrite(LED_ONBRD, !digitalRead(LED_ONBRD));
-
+                myAnimations->nextAnim(true);
             }
         }
         oldState = newState;
-    }
+        }
     IR_poll();
 }
