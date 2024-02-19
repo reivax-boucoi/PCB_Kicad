3be1e7f43c9be512538101549f5db3e8dfe7e1e3
reivax-boucoi <reivax.boucoi@laposte.net>
2024-02-19 22:00:47 +0100

    a

--- LED_poi/Software/test0/IR_cmd.h
+++ LED_poi/Software/test0/IR_cmd.h
@@ -2,11 +2,6 @@
 #define ir_cmd_h
 
 
-#define IR_SMALLD_SIRC12
-
-#define IR_PIN 0 // PD2 //TODO get actual pin in layout
-//Pinout: OUT GND VCC (similar to TSOP48xx)
-//Library only works with pins 0 RXD, 1 TXD, 2 SDA, 3 SCL, 7 PE6
 
 void IR_poll();
 
