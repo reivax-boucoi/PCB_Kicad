593d6c2b13f23b27db5b389a5ed62050b0e4ac63
reivax-boucoi <reivax.boucoi@laposte.net>
2024-02-21 22:00:31 +0100

    a

--- LED_poi/Software/test0/Animations.h
+++ LED_poi/Software/test0/Animations.h
@@ -6,6 +6,9 @@
 #define PIXEL_PIN 13 //PC7
 #define PIXEL_COUNT 7
 
+#define ANIM_COUNT 7
+#define ANIM_VERBOSE
+
 void LEDs_init();
 void LEDs_setColor(uint32_t c);
 void LEDS_showMode(uint8_t mode);
@@ -14,19 +17,22 @@ typedef void(*Anim)(uint8_t step);
 
 void Anim_blink(uint8_t step);
 void Anim_rainbow(uint8_t step);
+void Anim_quarters(uint8_t step);
 void Anim_blank(uint8_t step);
 
 class Animations {
 
     private:
-        uint8_t animId;
-        uint8_t animStep;
-        uint8_t animSpeed;
-        bool isEnabled;
-        Anim animList[7];
+        uint16_t animStep;
+        bool displayingAnimId;
+        bool accSyncEnabled;
+        Anim animList[ANIM_COUNT];
     public:
         Animations(void);
+        uint8_t animId;
+        uint16_t animSpeed;
         void setAnim(uint8_t new_animId);
+        void nextAnim(bool dir);
         void update();
 };
 
